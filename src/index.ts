f4fe1fe30c8897fc0a71e94e12995f0759bb5697
laura <carolinelaura69@gmail.com>
2024-09-02 18:26:25 -0300

    add console

--- src/index.ts
+++ src/index.ts
@@ -37,6 +37,7 @@ app.get('/api/product', async (req, res) => {
 app.get('/api/users', async (req, res) => {
   try{
     const products = await prisma.post.findMany()
+    console.log('x')
     return res.status(201).json(products); 
   }
   catch(err){
